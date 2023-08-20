f5bf7c8dc9abd5fd92993531dd448c94fa2b52e2
manipapan2 <manipapan2@gmail.com>
2023-08-20 21:40:33 +0330

    Updated

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 var khatcontainer = document.getElementById("khatcontainer")
+var khatasl = document.getElementById("khatasl")
+var khatbefore = document.querySelector(".khat-container div:nth-child(2)")
+var khatafter = document.querySelector(".khat-container div:nth-child(3)")
 var mobilemenucontainer = document.getElementById("mobilemenucontainer")
 
 var booli = false
@@ -6,10 +9,20 @@ var booli = false
 function bazobaste() {
     if(booli){
         mobilemenucontainer.style.transform="translateY(-250px)"
+        khatbefore.style.transform="translateY(-10px) rotate(0deg)"
+        khatbefore.style.width="21px"
+        khatafter.style.transform="translateY(10px) rotate(0deg)"
+        khatafter.style.width="21px"
+        khatasl.style.opacity="1"
         booli = false
     }
     else{
         mobilemenucontainer.style.transform="translateY(0px)"
+        khatbefore.style.transform="rotate(45deg)"
+        khatbefore.style.width="30px"
+        khatafter.style.transform="rotate(-45deg)"
+        khatafter.style.width="30px"
+        khatasl.style.opacity="0"
         booli = true
     }
 
